465b308e90a0560afd2721296c7c1c438b2f9c8d
Mapoet <16592606+Mapoet@users.noreply.github.com>
2025-03-01 17:09:30 +0000

    update

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/data")
+def data_page():
+    return "你已经打开了一个新的数据页"
